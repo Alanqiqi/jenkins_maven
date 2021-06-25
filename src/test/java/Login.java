60ad6ccc5c352413e3bc7fa95ef9055c3428dadf
zhangqi <2641703642@qq.com>
2021-06-25 18:29:14 +0800

    first

---
+++ src/test/java/Login.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class Login {
+    @Test
+    public void login(){
+        System.out.println("登录");
+    }
+}
---
+++ src/test/java/Regist.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class Regist {
+    @Test
+    public void  regist(){
+        System.out.println("注册");
+    }
+}
